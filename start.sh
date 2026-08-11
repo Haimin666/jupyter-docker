16b426c8bcbad0b66d02c302baaa9b42ddba026d
wanghaimin <w993932735@163.com>
2026-08-11 12:31:43 +0800

    fix: 门户经 changeOrigin 反代导致 Jupyter 跨源拦截登录，放行 corp.shiqiao.com 域名

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ exec jupyter lab \
     --port=8888 \
     --no-browser \
     --notebook-dir=/home/jovyan/work \
-    --ServerApp.base_url=/apps/jupyter
+    --ServerApp.base_url=/apps/jupyter \
+    --ServerApp.allow_origin_pat='https://.*\.corp\.shiqiao\.com'
